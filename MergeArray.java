79194cbe4080dbaa1518a3c266af5d3fcdeec937
Moksh <54073171+Moksh1999@users.noreply.github.com>
2019-12-09 21:05:51 +0530

    Update MergeArray.java

--- MergeArray.java
+++ MergeArray.java
@@ -46,5 +46,21 @@ public class MergeArray {
         {
             System.out.println(c[i]);
         }
+        
+         int n = c.length; 
+        for (int i = 0; i < n-1; i++) 
+            for (int j = 0; j < n-i-1; j++) 
+                if (c[j] > c[j+1]) 
+                { 
+                    
+                    int temp = c[j]; 
+                    c[j] = c[j+1]; 
+                    c[j+1] = temp; 
+                }
+        
+          for(int i=0;i<c.length;i++)
+        {
+            System.out.println(c[i]);
+        }
     }
 }
